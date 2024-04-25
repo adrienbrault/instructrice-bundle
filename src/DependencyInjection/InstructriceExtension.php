cbe8d73afec2447ce62513bd76599e72eb94256c
Baldur Rensch <baldurrensch@users.noreply.github.com>
2024-04-25 21:12:34 +0200

    Fix LLM Factory not getting APIKeys (#2)

--- src/DependencyInjection/InstructriceExtension.php
+++ src/DependencyInjection/InstructriceExtension.php
@@ -60,22 +60,24 @@ class InstructriceExtension extends ConfigurableExtension
      */
     protected function loadInternal(array $mergedConfig, ContainerBuilder $container): void
     {
+        $apiKeys = [
+            Anthropic::class => $mergedConfig['anthropic']['api_key'] ?? null,
+            Anyscale::class => $mergedConfig['anyscale']['api_key'] ?? null,
+            DeepInfra::class => $mergedConfig['deepinfra']['api_key'] ?? null,
+            Fireworks::class => $mergedConfig['fireworks']['api_key'] ?? null,
+            Groq::class => $mergedConfig['groq']['api_key'] ?? null,
+            Mistral::class => $mergedConfig['mistral']['api_key'] ?? null,
+            Ollama::class => $mergedConfig['ollama']['api_key'] ?? null,
+            OpenAi::class => $mergedConfig['openai']['api_key'] ?? null,
+            Perplexity::class => $mergedConfig['perplexity']['api_key'] ?? null,
+            Together::class => $mergedConfig['together']['api_key'] ?? null,
+        ];
+
         $definition = $container->register(Instructrice::class);
         $definition->setFactory(InstructriceFactory::class . '::create');
 
         $definition->setArguments([
-            '$apiKeys' => [
-                Anthropic::class => $mergedConfig['anthropic']['api_key'] ?? null,
-                Anyscale::class => $mergedConfig['anyscale']['api_key'] ?? null,
-                DeepInfra::class => $mergedConfig['deepinfra']['api_key'] ?? null,
-                Fireworks::class => $mergedConfig['fireworks']['api_key'] ?? null,
-                Groq::class => $mergedConfig['groq']['api_key'] ?? null,
-                Mistral::class => $mergedConfig['mistral']['api_key'] ?? null,
-                Ollama::class => $mergedConfig['ollama']['api_key'] ?? null,
-                OpenAi::class => $mergedConfig['openai']['api_key'] ?? null,
-                Perplexity::class => $mergedConfig['perplexity']['api_key'] ?? null,
-                Together::class => $mergedConfig['together']['api_key'] ?? null,
-            ],
+            '$apiKeys' => $apiKeys,
             '$llmFactory' => new Reference(LLMFactory::class),
             '$serializer' => new Reference('serializer'),
             '$propertyInfo' => new Reference('property_info'),
@@ -91,6 +93,7 @@ class InstructriceExtension extends ConfigurableExtension
         $definition->setArguments([
             '$client' => new Reference(SymfonyStreamingClient::class),
             '$logger' => new Reference('logger'),
+            '$apiKeys' => $apiKeys,
         ]);
 
         $container->setAlias('instructrice', Instructrice::class);
